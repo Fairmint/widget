fe0f2b53c90a840371cedaa401458dd13a8ea2aa
tmpacifitech <webmaster@motordirex.com>
2020-12-10 05:13:47 -0800

    Fix widget.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'commonjs': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module',
+    },
+    'rules': {
+        'semi': [ 'error', 'always' ],
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'always-multiline',
+            'exports': 'always-multiline',
+            'functions': 'never',
+        }],
+        'quotes': [ 'error', 'single' ],
+    },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    widget: './src/widget.js',
+    main: './src/main.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: (env === 'development') ? 'inline-source-map' : false,
@@ -19,9 +20,25 @@ module.exports = {
     port: 5000,
     hot: true,
   },
-  plugins: [new CopyPlugin({
-    patterns: [
-      { from: "demo" }
-    ]
-  })]
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'demo' },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
